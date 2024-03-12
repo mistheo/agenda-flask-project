99753e46c4704c8436e95bab59fe191da4cf49fe
mistheo <th.mistral@gmail.com>
2024-03-12 18:29:57 +0100

    Make Login page (and add application page)

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.get('/')
-def index():
-    return render_template('index.html')
 
-@app.get('/login')
-def login():
-        return render_template('login.html')
+@app.get("/")
+def displayIndexPage():
+    return render_template("index.html")
+
+
+@app.get("/login")
+def displayLoginPage():
+
+    isError = bool(request.args.get("failToLog"))
+    if isError:
+        return render_template("login.html", failToLog=True)
+    else:
+        return render_template("login.html", failToLog=False)
+
+
+@app.get("/application")
+def loadToApplication():
+    return render_template("appli.html")
